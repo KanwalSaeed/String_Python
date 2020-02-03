d5b0a0bfe2bf73e432cf23111709b975919738ab
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-03 18:27:34 +0500

    first code

---
+++ 4. STINGS IN PYTHON.py
@@ -0,0 +1,40 @@
+#  STINGS IN PYTHON
+
+# 1. SINGLE CODE
+first_Name = 'Kanwal'
+last_Name = 'Saeed'
+
+print(first_Name)
+print(last_Name)
+
+#DOUBLE CODE
+full_Name = "Kanwal Saeed"
+print(full_Name)
+country = "Pakiistan"
+print(country)
+
+#MULTIPLE LINES
+msg = """
+My name is Pakistan.
+I havve four Provinces Sindh, Punjaab, Balochistan, N.W.F.P.
+My founder is Quaid-e-Azam.
+He is a great Leader.
+"""
+print(msg)
+
+msgg = '''
+I am Karachi
+I am big city of Pakistan.
+I have a lot of beauty.
+'''
+print(msgg)
+
+
+cv = '''
+
+name : Kanwal
+Qualification : graduate
+Experiance : 4 years
+'''
+
+print(cv)
